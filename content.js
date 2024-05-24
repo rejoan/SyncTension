c605054a0bd52e8d14bab6355a9f906be41aad8c
Rejoan <rejoan.er@gmail.com>
2024-05-24 12:21:45 +0600

    synctension API works

--- content.js
+++ content.js
@@ -1,13 +1,55 @@
+var injections = 0;
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	openPopup(request).then((res) => {
-		var inputV = $('body').find('.el-dialog label:contains("Recharge Amount")').parents('.el-form-item').find('input');
-		inputV.val('1');
-		inputV[0].dispatchEvent(new CustomEvent('input'));
-		$('body').find('.el-dialog__wrapper:visible .el-dialog .el-dialog__footer button span:contains("Confirm")').click();
-    });
-	return true; 
-	
+	injections++;
+	var todo = request.todo;
+	if(todo  == 'deposit'){
+		todo = 'Recharge';
+	}else{
+		todo = 'Redeem';
+	}
+
+	if(injections == 1){
+		if(request.juser == 'reset'){
+			if($('body').find('.el-dialog label:contains("'+todo+' Amount")').parents('.el-form-item').find('input').length > 0){
+				$('body').find('.el-dialog label:contains("'+todo+' Amount")').parents('.el-form-item').find('input').val('');
+			}
+			$('body').find('.el-dialog__wrapper:visible .el-dialog button.el-dialog__headerbtn').click();
+			$('body').find('form.el-form div.el-form-item div.el-form-item__content button:contains("Reset")').click();		
+			return true;
+		}
+
+		openPopup(request).then(() => {
+			var inputV = $('body').find('.el-dialog label:contains("'+todo+' Amount")').parents('.el-form-item').find('input');
+			inputV.val('1');
+			inputV[0].dispatchEvent(new CustomEvent('input'));
+			$('body').find('.el-dialog__wrapper:visible .el-dialog .el-dialog__footer button span:contains("Confirm")').click();
+			//sendResponse({'action':todo,'username':request.juser,'rid':request.userid});
+		});
+	}
+	return true;
 });
+
+
+
+$('body').on('click', '.el-dialog__wrapper:visible .el-dialog .el-dialog__footer button span:contains("Confirm")', function(e){
+	var interval;
+    interval = setInterval(function(){
+      var zindex = $('.el-dialog__wrapper:visible').css('z-index');
+    	if(zindex >= 2004){
+			if(e.originalEvent){
+				var fm = e.originalEvent.pointerType;
+				if( fm.length > 0){
+					return;
+				}
+			}
+			$('.el-dialog__wrapper:visible .el-dialog[aria-label="dialog"] button.el-dialog__headerbtn').click();
+			$('body').find('form.el-form div.el-form-item div.el-form-item__content button:contains("Reset")').click();
+			clearInterval(interval);
+		}
+    }, 1000);
+	return true;
+});
+
 async function openPopup(request) {
 	if(!$('body').find('.el-dialog__wrapper').is(':visible')){
 		var inputF = $('body').find('*[placeholder="Please enter your search content"]');
@@ -17,7 +59,13 @@ async function openPopup(request) {
 		var dropLink = $('body').find('td:contains("'+request.juser+'")').parents('tr').find('.el-dropdown-link');
 		dropLink.click();
 		var ulID = dropLink.attr('aria-controls');
-		$('body').find('ul#'+ulID+' li:contains("Recharge")').click();	
+		var todo = request.todo;
+		if(todo  == 'deposit'){
+			todo = 'Recharge';
+		}else{
+			todo = 'Redeem';
+		}
+		$('body').find('ul#'+ulID+' li:contains("'+todo+'")').click();	
 	}
-	return true; 
+	return true;
 }
